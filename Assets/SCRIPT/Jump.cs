6c8c691a53e1cf265b14420b4a90df9b65403f02
Sushi <jasonshi08@gmail.com>
2021-09-08 18:33:50 -0400

    Adding Unity Files

---
+++ Assets/SCRIPT/Jump.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    public float thrust = 100f;
+    private bool isGrounded = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    
+
+	private void FixedUpdate()
+	{
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb2D.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+
+            
+            isGrounded = false;
+            
+        }
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.name == "floor")
+		{
+            isGrounded = true;
+		}
+	}
+}
---
+++ Assets/SCRIPT/Movement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 0f;
+    public float maxSpeed = 4f;
+    public float acceleration = 5f;
+    public float deceleration = 5f;
+    SpriteRenderer sr;
+
+   // Start is called before the first frame update
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left") && speed >  -maxSpeed)
+		{
+            speed = speed - acceleration * Time.deltaTime;
+		}
+        else if (Input.GetKey("right") && speed < maxSpeed)
+		{
+            speed = speed + acceleration * Time.deltaTime;
+		}
+		else
+		{
+            if (speed > deceleration * Time.deltaTime)
+			{
+                speed = speed - deceleration * Time.deltaTime;
+			}
+            else if (speed<-deceleration * Time.deltaTime)
+			{
+                speed = speed + deceleration * Time.deltaTime;
+			}
+			else
+			{
+                speed = 0;
+			}
+		}
+
+        transform.position = new Vector2 (transform.position.x + speed * Time.deltaTime, transform.position.y);
+    }
+
+    void FlipPlayer()
+    {
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        if (xMovement < -0.1f)
+        {
+            sr.flipX = true;
+        }
+        else if (xMovement > 0.1f)
+        {
+            sr.flipX = false;
+        }
+    }
+}
